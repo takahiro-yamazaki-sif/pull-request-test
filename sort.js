8d59006625d4fff9b0603ffeef41f0f3f9416814
takahiro-yamazaki-sif <takahiro.yamazaki@secure-i.jp>
2022-12-19 16:03:42 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,1 +1,11 @@
-var number = [19, 3, 81, 1, 24, 21]; console.log(number);
+var sortnumber = function (number) {
+    number.sort(function (a, b) {
+        if (a == b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
+    });
+};
+var number = [19, 3, 81, 1, 24, 21];
+sortnumber(number);
+console.log(number);
